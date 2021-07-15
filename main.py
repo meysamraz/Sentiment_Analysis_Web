2490003b10f694a1b1093248b37f5852a74a17de
Meysam Raz <mr.raz1381@gmail.com>
2021-07-15 16:09:54 +0430

    done the project

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask , render_template 
-
+from flask import Flask , render_template  ,request
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
 app = Flask(__name__)
-
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def main():
+    if request.method == "POST":
+        inp = request.form.get('inp')
+        sid = SentimentIntensityAnalyzer()
+        socre = sid.polarity_scores(inp)
+        if socre['neg'] != 0:
+            return render_template('home.html',message="Negative🙁🙁")
+        else:
+            return render_template('home.html',message="Positive🙂🙂")   
     return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
